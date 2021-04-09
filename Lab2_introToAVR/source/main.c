08b6e27801fe26d522eb7af6a224edd3f3010f31
Ethan Jackson <ethantjackson44@gmail.com>
2021-04-09 11:24:09 -0700

    Update main.c

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -13,20 +13,14 @@
 #endif
 
 int main(void) {
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-
-	unsigned char tmpB = 0x00;
-	unsigned char tmpA = 0x00;
+	DDRA = 0x00; PORTA = 0xFF; // A is input, writes 0xFF to A
+	DDRB = 0xFF; PORTB = 0x00; // B is output, initialize B to 0x00
+	unsigned char lightDetected = 0x00;
+	unsigned char doorOpen = 0x00; 
     while (1) {
-	tmpA = PINA & 0x01;
-	if (tmpA == 0x01) {
-		tmpB = (tmpB & 0xFC) | 0x01;
-	} else {
-		tmpB = (tmpB & 0xFC) | 0x02;
-	}
-	
-	PORTB = tmpB;
+	lightDetected = PINA & 0x02;
+	doorOpen = PINA & 0x01;
+	if (!lightDetected && doorOpen) PORTB = 0x01;
     }
     return 0;
 }
